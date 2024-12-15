ef93fb7a8c75fdc8a3c3e27b673f233543a46bc5
khalid tarmizi-alprog-D <khalidtarmizi01@gmail.com>
2024-12-15 12:21:30 +0800

    Add files via upload

---
+++ kholid9.py
@@ -0,0 +1,17 @@
+# suhu dalam celcius = 42
+# suhu dalam fahrenheit = 107.6
+# suhu dalam kelvin = 315.15
+# suhu dalam reamur = 33.6
+
+# suhu dalam celcius
+celcius = 42
+
+# rumus koversi suhu
+farenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", farenheit )
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
